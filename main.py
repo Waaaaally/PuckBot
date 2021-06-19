9f3007a008b22bbf191a03100f72fb39a3382d67
Waaaaally <pritishdas313@gmail.com>
2021-06-18 17:57:59 -0700

    pee#3 :pucknod: reaction rare% elo boosting spam to agreement infinity nirvana

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 # https://discordapp.com/api/oauth2/authorize?client_id=654139568588718090&permissions=116800&scope=bot Link to add puckbot.
+import random
+
 import discord
+import math
 
 f = open("token.txt", "r")
 token = f.read()
@@ -9,21 +12,29 @@ client = discord.Client()
 @client.event
 async def on_ready():
     print(f"{client.user} exists".format(client))
-
+#on message, receive message -> string -> print in console.
 
 @client.event
 async def on_message(message):
+
+    gamernumber = random.randint(1, 50)
+
     if message.author == client.user:
         return
-    elif message.content.startswith('+agree'):
+
+    elif message.content.startswith('+pucknod'):
         await message.channel.send("https://imgur.com/a/wtKXWjv")
+
+    elif "<:pucknod:801259360395329567>" in str(message.content) and gamernumber == 42: #funny haha 2% puckspam +pucknod
+
+        for i in range(10):
+            await message.channel.send("https://imgur.com/a/wtKXWjv")
 
     elif message.content.startswith('+disagree'):
         await message.channel.send("https://imgur.com/a/DJrsG3F")
 
     elif "+puck.dip" == message.content:
         await client.close()
-
 
 client.run(token)
 
