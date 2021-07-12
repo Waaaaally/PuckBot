7b4a93198fd06773fa5b9872829fe7fb0eda1f24
Waaaaally <pritishdas313@gmail.com>
2021-07-12 13:42:53 -0700

    pee#4 clown images, should add a command list at some point if I wanna continue. also birthdays eventually??

--- main.py
+++ main.py
@@ -26,12 +26,17 @@ async def on_message(message):
         await message.channel.send("https://imgur.com/a/wtKXWjv")
 
     elif "<:pucknod:801259360395329567>" in str(message.content) and gamernumber == 42: #funny haha 2% puckspam +pucknod
-
         for i in range(10):
             await message.channel.send("https://imgur.com/a/wtKXWjv")
 
     elif message.content.startswith('+disagree'):
         await message.channel.send("https://imgur.com/a/DJrsG3F")
+
+    elif message.content.startswith('+clown'):
+        if gamernumber%2 == 0:
+            await message.channel.send("https://imgur.com/a/aI8GMPn")
+        else:
+            await message.channel.send("https://imgur.com/a/MEDtGgs")
 
     elif "+puck.dip" == message.content:
         await client.close()
